62e0b30ff5023f7b182a4bcc76270ae005868f9c
paquerettee <paquerettee@gmail.com>
2025-11-19 10:52:55 +0100

    backend url updated

--- ebook-reader-frontend/src/config.js
+++ ebook-reader-frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://ebook-reader-9vca.onrender.com";
+export const BACKEND_URL = "https://ebook-reader-9vca.onrender.com/";
 // export const BACKEND_URL = "http://localhost:5000/";
 
 export const DB_NAME = "Read4MeDB";
